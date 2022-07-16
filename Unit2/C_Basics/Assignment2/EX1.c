9f68492dd5ced71b444039174e36584356a90220
Qamar-14 <104042729+Qamar-14@users.noreply.github.com>
2022-07-16 17:57:32 +0200

    C Program to check wether number is even or odd

---
+++ Unit2/C_Basics/Assignment2/EX1.c
@@ -0,0 +1,23 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+void main()
+{
+	int x ;
+
+	printf("Enter an integer you want to check: ");
+	fflush(stdout); fflush(stdin);
+	scanf("%d", &x);
+
+	if ((x % 2) ==0)
+	{
+		printf("%d is even",x);
+	}
+
+	else
+	{
+		printf("%d is odd",x);
+	}
+
+
+}
