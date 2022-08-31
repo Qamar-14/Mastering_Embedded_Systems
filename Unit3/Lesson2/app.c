cfeefa22d46960943af6b5d61a7c62e75530729c
Qamar-14 <104042729+Qamar-14@users.noreply.github.com>
2022-08-31 15:23:34 +0200

    Add files via upload

---
+++ Unit3/Lesson2/app.c
@@ -0,0 +1,7 @@
+#include "uart.h"
+
+unsigned char string_buffer[100] = "learn-in-depth:keroles" ;
+void main(void)
+{
+	Uart_Send_string (string_buffer);
+}
---
+++ Unit3/Lesson2/startup.s
@@ -0,0 +1,5 @@
+.global reset
+reset:
+    ldr sp, =0x0011000
+    bl main
+stop: b stop
