14b3471b90f61b3700b61a8a5faef9a714a55dbe
Qamar Elgohary <104042729+Qamar-14@users.noreply.github.com>
2022-09-27 15:21:30 +0200

    Delete Unit3/Lesson2 directory

--- Unit3/Lesson2/app.c
+++
@@ -1,7 +0,0 @@
-#include "uart.h"
-
-unsigned char string_buffer[100] = "learn-in-depth:keroles" ;
-void main(void)
-{
-	Uart_Send_string (string_buffer);
-}
--- Unit3/Lesson2/startup.s
+++
@@ -1,5 +0,0 @@
-.global reset
-reset:
-    ldr sp, =0x0011000
-    bl main
-stop: b stop
